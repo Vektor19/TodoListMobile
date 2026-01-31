1288be43b59a031747674fe8c323b05e803b2f9a
Vektor19 <viktor.komissarov@knu.ua>
2026-01-31 22:44:45 +0200

    init with lab1

---
+++ TodoListMobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace TodoListMobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
