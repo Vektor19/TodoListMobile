bce63a1a3ca43d96bb7498c2548d4ca98324cc4f
Vektor19 <viktor.komissarov@knu.ua>
2026-02-09 02:02:03 +0200

    make ui adaptive

--- TodoListMobile/MainPage.xaml.cs
+++ TodoListMobile/MainPage.xaml.cs
@@ -2,24 +2,55 @@
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
+            SizeChanged += OnPageSizeChanged;
+            UpdateVisualState();
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void OnPageSizeChanged(object sender, EventArgs e)
         {
-            count++;
+            UpdateVisualState();
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
+        private void UpdateVisualState()
+        {
+            double width = this.Width;
+            double height = this.Height;
+            
+            if (width <= 0 || height <= 0)
+            {
+                return;
+            }
+
+            string sizeState;
+            if (width >= 1200)
+                sizeState = "ExtraLargeScreen";
+            else if (width >= 900)
+                sizeState = "LargeScreen";
+            else if (width >= 600)
+                sizeState = "MediumScreen";
             else
-                CounterBtn.Text = $"Clicked {count} times";
+                sizeState = "SmallScreen";
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            string orientationState = width > height ? "Landscape" : "Portrait";
+
+            VisualStateManager.GoToState(MainLayout, sizeState);
+            VisualStateManager.GoToState(MainLayout, orientationState);
+
+            if (orientationState == "Landscape")
+            {
+                TaskLabel.WidthRequest = width * 0.65;
+                TaskEntry.WidthRequest = width * 0.65;
+                AddTaskBtn.WidthRequest = width * 0.65;
+            }
+            else
+            {
+                TaskLabel.WidthRequest = -1;
+                TaskEntry.WidthRequest = -1;
+                AddTaskBtn.WidthRequest = -1;
+            }
         }
     }
-
 }
